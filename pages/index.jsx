c81b7773abe3970d67056556a22cc8351ad761fa
YesImAk <61521239+YesImAk@users.noreply.github.com>
2023-06-12 03:27:18 +0530

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -42,7 +42,7 @@ export default function Index() {
         <Image width="600" height="600" src="/img/gift-left.png"  />
         </div>
         <div style={{ zIndex: '-1' }} className="hidden xl:block undrag opacity-10 pointer-events-none -right-[15vw] absolute top-[13vw] h-[90vw] grayscale -rotate-9">
-        <Image  width="600" height="600" src="/img/gift-right.png"  />
+        <Image  width="900" height="600" src="/img/gift-right.png"  />
         </div>
       </div>
 
