f0da2205258fbcff6d41ee1da3912ef368380277
YesImAk <61521239+YesImAk@users.noreply.github.com>
2023-06-12 00:29:29 +0530

    Update tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -4,9 +4,9 @@ export default function Tos() {
      <h2 className="mt-4 text-white font-bold text-3xl">Terms of Services</h2>
       <p className="text-sm text-white/30 mb-1">Last Update: 20/05/2022 🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-<p>⊳ Every discord server can use the Chill Beats for free, it is completely prohibited to sell or market for a fee.</p>
+<p>⊳ N/A.</p>
 <div className="py-3"></div>
-<p>⊳ You cannot use bot commands to harm the bot.</p>
+<p>⊳ Will be added soon</p>
 <div className="py-3"></div>
 </div>
     </div>
