a18f229dcff71ca153e4f7b2bccbef80c1d70d9c
YesImAk <61521239+YesImAk@users.noreply.github.com>
2023-06-12 03:00:22 +0530

    Update Footer.jsx

--- components/Static/Footer.jsx
+++ components/Static/Footer.jsx
@@ -57,20 +57,6 @@ export default function Footer() {
                                     </a>
                                 </Link>
                             </div>
-                            <div>
-                                <Link href="https://github.com/1umutda/MusicBot">
-                                    <a className="text-white/50 hover:text-white hover:underline transform duration-200">
-                                    <i className={`fa-brands fa-github`} /> GitHub 
-                                    </a>
-                                </Link>
-                            </div>
-                            <div>
-                                <Link href="https://youtube.com/UmutBayraktarYT">
-                                    <a className="text-white/50 hover:text-white hover:underline transform duration-200">
-                                    <i className={`fa-brands fa-youtube`} /> YouTube 
-                                    </a>
-                                </Link>
-                            </div>
                         </div>
                     </div>
                     <div className="mt-10 grid content-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
