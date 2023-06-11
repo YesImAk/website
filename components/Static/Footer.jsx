8a2d45dfdf3f8a4ef55d97df07f21056cfab119b
YesImAk <61521239+YesImAk@users.noreply.github.com>
2023-06-12 03:09:03 +0530

    Update Footer.jsx

--- components/Static/Footer.jsx
+++ components/Static/Footer.jsx
@@ -13,25 +13,7 @@ export default function Footer() {
                             <div className="flex items-center space-x-5">
                                 <img src="/img/logo2.png" className="rounded-full w-12" />
                                 <p className="font-semibold text-xl text-white">Chill Beats</p>
-                            </div>
-                            <Menu as="div" className="relative mt-3 inline-block text-left">
-                                <div>
-                                    <Menu.Button className="flex z-1 items-center w-full px-4 py-2 text-sm font-medium text-white bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-                                        <img
-                                            src={`https://flagcdn.com/w80/gb.png`}
-                                            width="24"
-                                            height="18"
-                                            className="mr-2 h-4 rounded-sm"
-                                        />
-                                        English
-                                        <ChevronDownIcon
-                                            className="w-5 h-5 ml-2 -mr-1 transform rotate-180 text-violet-200 hover:text-violet-100"
-                                            aria-hidden="true"
-                                        />
-                                    </Menu.Button>
-                                </div>
-                            </Menu>
-                        
+                            </div>                      
                         </div>
                         <div className="col-span-1">
                             <p className="text-white font-medium mt-3 sm:mt-0 sm:mb-3">Links</p>
