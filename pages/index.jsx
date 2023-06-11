fd770e9cc522aa0495c06147bf100d319bab5924
YesImAk <61521239+YesImAk@users.noreply.github.com>
2023-06-12 03:24:47 +0530

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -17,13 +17,23 @@ export default function Index() {
 	  </p>
         <div className="animateHeader mt-10 flex flex-wrap items-center justify-center gap-x-4">
   
-            <Link href={"https://shorturl.at/prY36"}>
+            <Link href={"https://shorturl.at/zKNX7"}>
               <a
                 className={
                   "flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " 
                 }
               >
           Invite Chill Beats
+              </a>
+            </Link>
+
+            <Link href={"https://shorturl.at/prY36"}>
+              <a
+                className={
+                  "flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " 
+                }
+              >
+          Invite Chill Beats Legacy
               </a>
             </Link>
         </div>
