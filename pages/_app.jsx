c4433e2db0a7d4e78310f2d5bbf88ca76f8c1e9a
YesImAk <61521239+YesImAk@users.noreply.github.com>
2023-06-15 21:06:35 +0530

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -46,7 +46,7 @@ export default function AwardApp({ Component, pageProps }) {
     <div className="h-screen relative border-t-4 border-amber-600">
       <div
         className="bg-gradient-to-b z-10 opacity-[25%] absolute top-0 w-full from-amber-600 to-transparent"
-        style={{ height: "500px" }}
+        style={{ height: "1000px" }}
       />
       <Head>
         <title>
