7f7786843d29a737b48c91e120c1f83fa661e4dd
YesImAk <61521239+YesImAk@users.noreply.github.com>
2023-06-15 20:56:17 +0530

    Update Footer.jsx

--- components/Static/Footer.jsx
+++ components/Static/Footer.jsx
@@ -15,21 +15,6 @@ export default function Footer() {
                                 <p className="font-semibold text-xl text-white">Chill Beats</p>
                             </div>                      
                         </div>
-                        <div className="col-span-1">
-                            <p className="text-white font-medium mt-3 sm:mt-0 sm:mb-3">Links</p>
-                            <div>
-                                <Link href="#">
-                                    <a className="text-white/50 hover:text-white hover:underline transform duration-200">
-                                        Vote Bot
-                                    </a>
-                                </Link>
-                            </div>
-                            <div>
-                                <a href="https://shorturl.at/zKNX7" target="_blank" className="text-white/50 hover:text-white hover:underline transform duration-200">
-                                    Invite Bot
-                                </a>
-                            </div>
-                        </div>
                     </div>
                     <div className="mt-10 grid content-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
                         <p className="text-white text-center sm:text-left text-opacity-50">
