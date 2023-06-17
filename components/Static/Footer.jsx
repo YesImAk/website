bd6b6622defb1263f9f44a0d5391e75e73c9ac26
YesImAk <61521239+YesImAk@users.noreply.github.com>
2023-06-17 19:16:52 +0530

    Update Footer.jsx

--- components/Static/Footer.jsx
+++ components/Static/Footer.jsx
@@ -9,11 +9,7 @@ export default function Footer() {
             <footer className="py-10">
                 <div className="pt-10 mx-auto">
                     <div className="lg:grid lg:grid-cols-6 gap-20">
-                        <div className="col-span-3">
-                            <div className="flex items-center space-x-5">
-                                <img src="/img/logo2.png" className="rounded-full w-12" />
-                                <p className="font-semibold text-xl text-white">Chill Beats</p>
-                            </div>                      
+                        <div className="col-span-3">                  
                         </div>
                     </div>
                     <div className="mt-10 grid content-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
