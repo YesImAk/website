a1b5e4e8357c32338b79a29f47499bacc4123d08
YesImAk <61521239+YesImAk@users.noreply.github.com>
2023-06-20 01:54:17 +0530

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -82,11 +82,11 @@ export default function Index() {
       <div className="flex flex-row w-full h-auto lg:h-56 my-14 lg:my-40 bg-gradient-to-br lg:px-30 from-neutral-900/90 flex items-center justify-start to-neutral-900/50 rounded-lg p-1 shadow-md">
               <>
                 <div className="lg:mx-4 lg:p-8 py-4 lg:py-16 flex items-center justify-center flex-col text-center w-full lg:w-auto lg:text-left lg:flex-none">
-                  <img src="https://cdn.discordapp.com/attachments/1116188178336985089/1118923760196653126/image.png"  width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" />
+                  <img src="https://cdn.discordapp.com/attachments/1116188178336985089/1120448787169681438/image.png"  width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" />
                   <p className="text-3xl text-white font-semibold">Best Your Bot</p>
                   <p className="text-md text-white font-medium text-gray-500 line-clamp-5">Are you ready to share your playlist with Chill Beats?</p>
                 </div>
-                <img src="https://cdn.discordapp.com/attachments/1116188178336985089/1118923760196653126/image.png" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
+                <img src="https://cdn.discordapp.com/attachments/1116188178336985089/1120448787169681438/image.png" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
               </>
       
         </div>
