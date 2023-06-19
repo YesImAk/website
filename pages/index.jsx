dd248ddae654888e587adc5a2e0d2008fa2791c8
YesImAk <61521239+YesImAk@users.noreply.github.com>
2023-06-19 23:29:47 +0530

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,7 +7,7 @@ export default function Index() {
     <>
       <div className="py-20 mb-30 max-w-3xl mx-auto">
         <div className="flex w-full justify-center">
-	 <img width="250" src="https://i.imgur.com/0NVC52Y.png" />
+	 <img width="500" src="https://i.imgur.com/0NVC52Y.png" />
       </div>
         <p className="animateHeader text-4xl font-extrabold text-center text-white">
           Chill Beats
