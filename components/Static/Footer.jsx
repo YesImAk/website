cf015fdde3ac166eb1504e53983534aaa1116d96
YesImAk <61521239+YesImAk@users.noreply.github.com>
2023-06-20 02:29:21 +0530

    Update Footer.jsx

--- components/Static/Footer.jsx
+++ components/Static/Footer.jsx
@@ -9,15 +9,11 @@ export default function Footer() {
             <footer className="py-10">
                 <div className="pt-10 mx-auto">
                     <div className="lg:grid lg:grid-cols-6 gap-20">
-                        <div className="col-span-3">                  
-                        </div>
                     </div>
                     <div className="mt-10 grid content-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
                         <p className="text-white text-center sm:text-left text-opacity-50">
                             {new Date().getFullYear()} &copy; Chill Beats
                         </p>
-                        <div className="hidden md:flex items-center justify-center">
-                        </div>
                     </div>
                 </div>
             </footer>
