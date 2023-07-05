70e1f6d649f167908bdc69744f3697ebbb63a6c4
YesImAk <61521239+YesImAk@users.noreply.github.com>
2023-07-05 20:27:21 +0530

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,14 +11,14 @@ class MyDocument extends Document {
           <meta name="googlebot" content="index,follow" />
           <meta
             name="description"
-            content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
+            content="Invite Chill Bots today and start listining to music without any lag."
           />
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:site" content="@MusicMaker" />
           <meta name="twitter:creator" content="@MusicMaker" />
-          <meta property="og:url" content="https://musicmaker.vercel.app" />
+          <meta property="og:url" content="https://www.chillbots.online/" />
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="MusicMaker" />
+          <meta property="og:title" content="Chill Beats" />
           <link
             rel="icon"
             href="/img/logo2.png"
@@ -26,14 +26,14 @@ class MyDocument extends Document {
           />
           <meta
             property="og:description"
-            content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
+            content="Invite Chill Bots today and start listining to music without any lag."
           />
           <meta property="og:image" content="/img/logo.png" />
           <meta property="og:image:alt" content="MusicMaker" />
           <meta property="og:locale" content="en_GB" />
           <meta
             property="og:site_name"
-            content="MusicMaker"
+            content="Chill Beats"
           />
           <meta name="theme-color" content="#6108dd" />
           <link rel="icon" href="/img/logo.png" type="image/x-icon" />
