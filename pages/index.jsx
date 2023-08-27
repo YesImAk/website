9769fa92cf51ad3ddaaf75fdd725a0bc88c8683f
YesImAk <61521239+YesImAk@users.noreply.github.com>
2023-08-27 21:24:36 +0530

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -83,8 +83,7 @@ export default function Index() {
               <>
                 <div className="lg:mx-4 lg:p-8 py-4 lg:py-16 flex items-center justify-center flex-col text-center w-full lg:w-auto lg:text-left lg:flex-none">
                   <img src="https://cdn.discordapp.com/attachments/1116188178336985089/1120448787169681438/image.png"  width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" />
-                  <p className="text-3xl text-white font-semibold">Best Your Bot</p>
-                  <p className="text-md text-white font-medium text-gray-500 line-clamp-5">Are you ready to share your playlist with Chill Beats?</p>
+                  <p className="text-3xl text-white font-semibold">Best Bot</p>
                 </div>
                 <img src="https://cdn.discordapp.com/attachments/1116188178336985089/1120448787169681438/image.png" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
               </>
